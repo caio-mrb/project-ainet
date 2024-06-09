2dd1c8f18ed10df39f1530f5d54d6096160b566f
Caio Barbosa <caiomaxwel@hotmail.com>
2024-06-09 11:38:35 +0100

    Provisional components

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -117,8 +117,9 @@
       </header>
       <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <!-- Your content -->
+
+        
+            {{$text}}
         </div>
       </main>
-    {{$text}}
 </div>
