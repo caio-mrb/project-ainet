b46d26b3bbc395858331b973b35980099dfd9f8e
Caio Barbosa <caiomaxwel@hotmail.com>
2024-06-14 20:07:52 +0100

    Update

--- resources/views/components/movies/table.blade.php
+++ resources/views/components/movies/table.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full flex justify-items-center justify-center {{ $attributes }}">
-<div class="flex flex-row flex-wrap gap-4 w-3/4" >
+<div class="flex flex-row flex-wrap gap-4 md:w-full lg:w-3/4" >
     @foreach($movies as $movie)
         <x-movies.card :movie="$movie"></x-movies>
     @endforeach
